96d869a643993bbb5ecf9c2ff4b781f96bc45aef
DarkPixel100 <diegofavila20@gmail.com>
2023-10-27 09:18:34 -0300

    CPF validation

--- subscriptions/tests/test_form_subscribe.py
+++ subscriptions/tests/test_form_subscribe.py
@@ -4,9 +4,38 @@ from subscriptions.forms import SubscriptionForm
 
 
 class SubscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_fields(self):
+        self.form = SubscriptionForm()
         self.assertSequenceEqual(
             ['name', 'cpf', 'email', 'phone'], list(self.form.fields))
+
+    def test_cpf_has_digit(self):
+        form = self.make_validated_form(cpf='ABC45678901')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+
+    def test_cpf_has_11_digits(self):
+        form = self.make_validated_form(cpf='123456')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        error_list = errors['cpf']
+        exception = error_list[0]
+        self.assertEqual(code, exception.code)
+        
+
+    def assertFormErrorMessage (self, form, field, msg):
+        errors = form.errors
+        error_list = errors[field]
+        self.assertListEqual([msg], error_list)
+
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(name="Diego",
+                    cpf="ABC45678901",
+                    email="diego.avila@aluno.riogrande.ifrs.edu.br",
+                    phone="53-99101-1002")
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
