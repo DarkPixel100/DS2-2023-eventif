2dc7edd42e1d15282f7a3eafc4ec636e29b29879
Diego Fontes de Avila <49082448+DarkPixel100@users.noreply.github.com>
2023-12-04 13:27:04 +0000

    "name" virou "nome"

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm (forms.Form):
-    name = forms.CharField(label="Name")
+    name = forms.CharField(label="Nome")
     phone = forms.CharField(label="Telefone", empty_value="Não inserido")
     email = forms.EmailField(label="Email")
     message = forms.CharField(label="Mensagem", widget=forms.Textarea)
