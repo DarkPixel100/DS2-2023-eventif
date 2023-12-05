5600e161f6df9b9b30dd6e82f7ad2cb8fb21a1d1
DarkPixel100 <diegofavila20@gmail.com>
2023-12-05 20:47:53 -0300

    Responder / Marcar como "Respondido"

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 from contact.models import Contact
 
 
@@ -7,14 +8,46 @@ class ContactModelAdmin(admin.ModelAdmin):
         "name",
         "email",
         "phone",
-        "message",
         "created_at",
+        "updated_at",
+        "replied",
     )
     date_hierarchy = "created_at"
-    search_fields = ["name", "email", "phone", "message", "created_at"]
-    list_filter = [
+    search_fields = [
+        "name",
+        "email",
+        "phone",
+        "message",
         "created_at",
+        "updated_at",
+        "response",
     ]
+    list_filter = ["created_at", "updated_at", "replied"]
+
+    actions = ["mark_as_replied"]
+
+    def mark_as_replied(self, request, queryset):
+        count = queryset.update(replied=True)
+
+        if count == 1:
+            msg = "{} mensagem foi respondida."
+        else:
+            msg = "{} mensagens foram respondidas."
+
+        self.message_user(request, msg.format(count))
+
+    mark_as_replied.short_description = "Marcar como \"Respondido\""
+
+    def save_model(self, request, obj, form, change):
+        if not obj.replied and "response" in form.changed_data and obj.response.strip():
+            obj.replied = True
+            form.changed_data.append("replied")
+
+        if "updated_at" not in form.changed_data:
+            obj.updated_at = timezone.now()
+            form.changed_data.append("updated_at")
+
+        obj.save(update_fields=form.changed_data)
 
 
 admin.site.register(Contact, ContactModelAdmin)
--- contact/tests/test_admin_contact.py
+++ contact/tests/test_admin_contact.py
@@ -1,7 +1,5 @@
 from unittest.mock import Mock
-
 from django.test import TestCase
-
 from contact.admin import ContactModelAdmin, Contact, admin
 
 
@@ -14,4 +12,40 @@ class ContactModelAdminTest(TestCase):
             message="Mensagem teste",
         )
 
-        self.model_admin = ContactModelAdmin(Contact, admin.site)
+        self.contact_model_admin = ContactModelAdmin(Contact, admin.site)
+
+    def test_has_mark_as_replied_action(self):
+        self.assertIn("mark_as_replied", self.contact_model_admin.actions)
+
+    def test_action_updates_replied_flag(self):
+        # Arrange
+        queryset = Contact.objects.all()
+
+        # Act
+        self.call_mark_as_replied_action(queryset)
+
+        # Assert
+        self.assertEqual(Contact.objects.filter(replied=True).count(), 1)
+
+    def test_action_sends_correct_message(self):
+        # Arrange
+        queryset = Contact.objects.all()
+
+        # Act
+        self.call_mark_as_replied_action(queryset)
+
+        # Assert
+        expected_message = "1 mensagem foi respondida."
+        self.mock.assert_called_once_with(None, expected_message)
+
+    def call_mark_as_replied_action(self, queryset):
+        # Arrange
+        self.mock = Mock()
+        old_message_user = ContactModelAdmin.message_user
+        ContactModelAdmin.message_user = self.mock
+
+        # Act
+        self.contact_model_admin.mark_as_replied(None, queryset)
+
+        # Clean up
+        ContactModelAdmin.message_user = old_message_user
